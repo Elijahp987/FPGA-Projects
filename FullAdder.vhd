f9e64a4b6efc24939bf0a33568e6726361490539
Elijahp987 <156743540+Elijahp987@users.noreply.github.com>
2024-09-18 14:41:36 -0400

    Add files via upload

---
+++ FullAdder.vhd
@@ -0,0 +1,50 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 09/01/2024 01:56:36 PM
+-- Design Name: 
+-- Module Name: FullAdder - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity FullAdder is
+    Port ( A : in STD_LOGIC;
+           B : in STD_LOGIC;
+           Cin : in STD_LOGIC;
+           S : out STD_LOGIC;
+           Cout : out STD_LOGIC);
+end FullAdder;
+
+architecture Behavioral of FullAdder is
+
+begin
+
+S <= A xor B xor Cin;
+Cout <= (A and B) or (A and Cin) or (B and Cin);
+
+
+end Behavioral;
